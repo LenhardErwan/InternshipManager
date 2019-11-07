c199d55424202b848e95d993b3e7792e52364295
Jeremy-JRM <39135270+Jeremy-JRM@users.noreply.github.com>
2019-11-07 23:02:12 +0100

    Update ConfDB.inc.php

--- src/assets/script/ConfDB.inc.php
+++ src/assets/script/ConfDB.inc.php
@@ -1,13 +1,13 @@
 <?php
-    define('DB_HOST', 'localhost');
+    define('DB_HOST', 'jeremy-jrm.net');
     define('DB_PORT', '5432');
-    define('DB_DATABASE', 'BDD_Avances');
-    define('DB_SCHEMA', 'projet_php');
-    define('DB_USERNAME', 'test');
-    define('DB_PASSWORD', 'test');
+    define('DB_DATABASE', 'projetPHP');
+    define('DB_SCHEMA', 'internshipmanager');
+    define('DB_USERNAME', 'projetPHP');
+    define('DB_PASSWORD', 'bQER5NUs5636BYeBdCRbmEhEkUJinh8z');
 
     try {
-        $DB = new PDO('pgsql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE.';user='.DB_USERNAME.' password='.DB_PASSWORD);
+        $DB = new PDO('pgsql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE.';user='.DB_USERNAME.';password='.DB_PASSWORD);
         $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $DB->exec("SET NAMES 'utf8'; SET search_path TO ".DB_SCHEMA.";");
     } catch (Exception $e) {
