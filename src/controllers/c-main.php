746404df615fec71a61a9a2a7aeb9e1e2806323e
LenhardErwan <lenhard.erwan@gmail.com>
2019-11-17 16:56:34 +0100

    Complete vote system

--- src/controllers/c-main.php
+++ src/controllers/c-main.php
@@ -44,15 +44,17 @@
             $id_account = (isset($_SESSION['id_user']) && !empty($_SESSION['id_user'])) ? $_SESSION['id_user'] : null;
             $type = (isset($_REQUEST['type']) && !empty($_REQUEST['type'])) ? $_REQUEST['type'] : null;
             $id_hash = (isset($_REQUEST['id_hash']) && !empty($_REQUEST['id_hash'])) ? $_REQUEST['id_hash'] : null;
-            
-            if(!is_null($id_hash)) $article = Article::getArticle($id_hash);
-            
-            if(isset($article) && $article) {
-                $id_article = $article->id_article;
-            }
 
-            if(!is_null($id_account) && !is_null($article) && !is_null($type)) {
-                Article::voteFor($id_account, $id_article, $type);
+            if(!Article::isCompany($id_account)) {  //Companies cannot vote
+                if(!is_null($id_hash)) $article = Article::getArticle($id_hash);
+            
+                if(isset($article) && $article) {
+                    $id_article = $article->id_article;
+                }
+
+                if(!is_null($id_account) && !is_null($article) && !is_null($type)) {
+                    Article::voteFor($id_account, $id_article, $type);
+                }
             }
             
             $votes = Article::getNbVotes($id_article);
