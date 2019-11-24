f701e81607e4d602fedb1cb1d7836b052520b33b
Jeremy-JRM <39135270+Jeremy-JRM@users.noreply.github.com>
2019-11-24 18:26:36 +0100

    Update v-nav stylesheet

--- src/views/v-nav.inc.php
+++ src/views/v-nav.inc.php
@@ -12,29 +12,33 @@
 			<a href="?page=admin" class="nav_button">Administration</a>
 		<?php } ?>
 		<a href="?page=profile&id=<?= $_SESSION['id_account']; ?>" class="nav_button">Mon profil</a>
-		<a href="?page=settings" class="nav_button">Parametres</a>
+		<a href="?page=profile&action=edit_profile&id=<?= $_SESSION['id_account']; ?>" class="nav_button">Parametres</a>
 		<a href="?page=disconnect" class="nav_button">Deconnexion</a>
 	<?php } ?>
 </nav>
 <?php
 	if(!isset($_SESSION['id_account'])) {
 ?>
-	<div class="modal" id="connect_form" style="display: <?php echo(isset($_POST['con_submit']) ? 'block' : 'none')?>;">
-		<div id="connect_container">
-			<form action="" method="POST">
-				Mail : <input type="email" name="con_mail" maxlength="80" value="<?php if(isset($_POST['con_mail'])) { echo $_POST['con_mail']; } ?>" required>
-				<span><?php if(isset($errors['mail'])) { echo $errors['mail']; } ?></span>
-				<br/>
+	<div class="modal" id="connect_form" style="display: <?php echo(isset($_POST['con_submit']) ? 'flex' : 'none')?>;">
+		<form id="connect_container" action="" method="POST">
+			<div class="connect_elmt">
+				<label>Mail : </label>
+				<input type="email" name="con_mail" maxlength="80" value="<?php if(isset($_POST['con_mail'])) { echo $_POST['con_mail']; } ?>" required>
+			</div>
+			<div class="connect_errors"><?php if(isset($errors['mail'])) { echo $errors['mail']; } ?></div>
 
-				Mot de passe : <input type="password" name="con_password" maxlength="64" required>
-				<span><?php if(isset($errors['password'])) { echo $errors['password']; } ?></span>
-				<br/>
+			<div class="connect_elmt">
+				<label>Mot de passe : </label>
+				<input type="password" name="con_password" maxlength="64" required>
+			</div>
+			<div class="connect_errors"><?php if(isset($errors['password'])) { echo $errors['password']; } ?></div>
 
-				<span><?php if(isset($errors['active'])) { echo $errors['active']; } ?></span>
+			<div class="connect_errors"><?php if(isset($errors['active'])) { echo $errors['active']; } ?></div>
+			<div id="connect_submit">
 				<button name="con_submit">Connexion</button>
-			</form>
-    		<button class="close_modal">Annuler</button>
-    	</div>
+    			<button class="close_modal">Annuler</button>
+    		</div>
+		</form>
 	</div>
 <?php
  	}
