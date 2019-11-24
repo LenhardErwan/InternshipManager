a5d886b39a9b8b8de5f9e248824aaa59c6fedfd3
LenhardErwan <lenhard.erwan@gmail.com>
2019-11-24 20:40:21 +0100

    Fix delete_account

--- src/controllers/c-profile.php
+++ src/controllers/c-profile.php
@@ -249,7 +249,7 @@
                 $account = User::getAccountByID($id_user);
                 $can_delete = $id_user == $id_account || $status == "admin";
                 if($account && $can_delete) {
-                    Article::deleteComment($id_user);
+                    User::deleteAccount($id_user);
                     header('Location: index.php');
                     exit();
                 }
