3db065384d7e484e6c208883c700c05ab0165450
LenhardErwan <lenhard.erwan@gmail.com>
2019-11-25 11:29:27 +0100

    Update v-vote.inc.php

--- src/views/v-vote.inc.php
+++ src/views/v-vote.inc.php
@@ -2,6 +2,8 @@
     <span>Nombre de like : <?= $votes['positive'] ?></span><br/>
     <span>Nombre de dislike : <?= $votes['negative'] ?></span><br/>
     <span>Valeur des votes : <?= ($votes['positive'] - $votes['negative'] ) ?>, sur un total de <?= ($votes['positive'] + $votes['negative'] ) ?> votes</span><br/>
+    <?php if($status != "company") { ?>
     <button class="like" onclick="<?= $functions['like']; ?>" <?php if(isset($user_vote) && $user_vote->type) echo "check"; ?>>like</button>
     <button class="dislike" onclick="<?= $functions['dislike']; ?>" <?php if(isset($user_vote) && !$user_vote->type) echo "check"; ?>>dislike</button>
+    <?php } ?>
 </div>
