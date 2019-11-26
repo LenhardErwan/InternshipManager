cdf09e3e3cd933f88f5867f79b633c2a61a280e7
Jeremy-JRM <39135270+Jeremy-JRM@users.noreply.github.com>
2019-11-26 22:33:19 +0100

    comment style

--- src/views/v-comment.inc.php
+++ src/views/v-comment.inc.php
@@ -3,8 +3,8 @@
     <p><?= nl2br($comment->text) ?></p>
     <?php if($status == "admin") { ?>
     <form action="" method="POST">
-        <button type="submit" class="article_comment_button" id="edit_comment" name="action" value="edit_comment">Editer</button>
-    	<button type="button" class="open_modal article_comment_button" onClick="openModal('delete_comment')">Supprimer</button>
+        <button type="submit" class="article_comment_button" id="edit_comment" name="action" value="edit_comment">Modifier le commentaire</button>
+    	<button type="button" class="open_modal article_comment_button" onClick="openModal('delete_comment')">Supprimer le commentaire</button>
     </form>
     <?php } ?>
 </div>
